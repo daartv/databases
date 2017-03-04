79d75ca987ab0f1a5c4ed7bb5eed0c514619e049
daartv <daartv@gmail.com>
2017-03-03 19:26:25 -0800

    Create messages schema

--- server/schema.sql
+++ server/schema.sql
@@ -4,6 +4,11 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `username` MEDIUMTEXT NULL DEFAULT NULL,
+  `text` MEDIUMTEXT NULL DEFAULT NULL,
+  `room` MEDIUMTEXT NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
 );
 
 /* Create other tables and define schemas for them here! */
@@ -15,3 +20,42 @@ CREATE TABLE messages (
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
 
+/* -- ---
+-- Globals
+-- ---
+
+-- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- SET FOREIGN_KEY_CHECKS=0;
+
+-- ---
+-- Table 'Messages'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `Messages`;
+    
+CREATE TABLE `Messages` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `username` MEDIUMTEXT NULL DEFAULT NULL,
+  `text` MEDIUMTEXT NULL DEFAULT NULL,
+  `room` MEDIUMTEXT NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Foreign Keys 
+-- ---
+
+
+-- ---
+-- Table Properties
+-- ---
+
+-- ALTER TABLE `Messages` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ---
+-- Test Data
+-- ---
+
+-- INSERT INTO `Messages` (`id`,`username`,`text`,`room`) VALUES
+-- ('','','',''); */ 
