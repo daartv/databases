9b2542f081e23a565379609c8bbe476cabc2382e
daartv <daartv@gmail.com>
2017-03-03 20:19:53 -0800

    Add database name to tests

--- server/schema.sql
+++ server/schema.sql
@@ -1,21 +1,14 @@
-CREATE DATABASE chat;
-
+-- CREATE DATABASE chat;
 USE chat;
-
 CREATE TABLE messages (
   /* Describe your table here.*/
-  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `username` MEDIUMTEXT NULL DEFAULT NULL,
-  `text` MEDIUMTEXT NULL DEFAULT NULL,
-  `room` MEDIUMTEXT NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  id INT,
+  username VARCHAR(15),
+  text VARCHAR(120),
+  room VARCHAR(10),
+  PRIMARY KEY (id)
 );
-
 /* Create other tables and define schemas for them here! */
-
-
-
-
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
